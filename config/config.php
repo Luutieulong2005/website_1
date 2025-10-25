1ccec413e823b19e81445a947cf467e8f8e8ca50
Luutieulong2005 <23211tm2241@mail.tdc.edu.vn>
2025-10-25 12:38:47 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,13 +1,13 @@
 <?php
 $configDB = array();
-$configDB["host"] 		= "localhost";
+$configDB["host"] 		= "mysql-lab-db.mysql.database.azure.com";
 $configDB["database"]	= "computer_store";
-$configDB["username"] 	= "root";
-$configDB["password"] 	= "";
-define("HOST", "localhost");
+$configDB["username"] 	= "sqladmin";
+$configDB["password"] 	= "Long2209@";
+define("HOST", "mysql-lab-db.mysql.database.azure.com");
 define("DB_NAME", "computer_store");
-define("DB_USER", "root");
-define("DB_PASS", "");
+define("DB_USER", "sqladmin");
+define("DB_PASS", "Long2209@");
 define('ROOT', dirname(dirname(__FILE__) ) );
 //Thu muc tuyet doi truoc cua config; c:/wamp/www/lab/
 define("BASE_URL", "http://".$_SERVER['SERVER_NAME']);//dia chi website
