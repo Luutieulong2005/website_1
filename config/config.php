4184092e10f6540f95540d362fd0b8870efc9d81
Luutieulong2005 <23211tm2241@mail.tdc.edu.vn>
2025-11-03 16:06:14 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -4,8 +4,8 @@ define('ROOT', dirname(dirname(__FILE__)));
 // CẤU HÌNH DATABASE CỦA BẠN - SỬA THÔNG TIN NÀY
 $host = 'mysql-lab-db.mysql.database.azure.com';  // Host của bạn
 $dbname = 'lienquan_shop';                        // Tên database
-$username = 'username_cua_ban';                   // SỬA: username của bạn
-$password = 'password_cua_ban';                   // SỬA: password của bạn
+$username = 'sqladmin';                   // SỬA: username của bạn
+$password = 'Long2209@';                   // SỬA: password của bạn
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
